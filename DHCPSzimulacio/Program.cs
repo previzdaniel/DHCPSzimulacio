78dad4c4ddeba89295de9dff48f38cae28213e1e
previzdaniel <dani.2000.2x@gmail.com>
2020-10-08 09:24:27 +0200

    CimEgyenlo metódus elkészítése

--- DHCPSzimulacio/Program.cs
+++ DHCPSzimulacio/Program.cs
@@ -9,8 +9,73 @@ namespace DHCPSzimulacio
 {
     class Program
     {
+        static List<string> excluded = new List<string>();
+        static Dictionary<string, string> reserved = new Dictionary<string, string>();
+
+        static void BeolvasExcluded()
+        {
+            try
+            {
+                StreamReader file = new StreamReader("excluded.csv");
+                try
+                {
+                    while (!file.EndOfStream)
+                    {
+                        excluded.Add(file.ReadLine());
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                finally
+                {
+                    file.Close();
+                }
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static string CimEgyenlo(string cim)
+        {
+            /*
+            cim = "192.168.10.100"
+            return "192.168.10.101"
+
+            szétvágni"."
+            az utolsót inté konvertálni
+            egyet hozzáadni (255 ne lépjük túl)
+
+            összefűzni stringé
+            */
+              string[] adatok = cim.Split('.');
+              int okt4 = Convert.ToInt32(adatok[3]);
+              if (okt4 < 255)
+              {
+                   okt4++;
+              }
+            return adatok[0] + "." + adatok[1] + "." + adatok[2] + "." + okt4.ToString();
+        }
+
         static void Main(string[] args)
         {
+            BeolvasExcluded();
+
+            //foreach (var e in excluded)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            Console.WriteLine(CimEgyenlo("192.168.10.100"));
+
+            Console.WriteLine("\nvége...");
+
+            Console.ReadKey();
         }
     }
 }
